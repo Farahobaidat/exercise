1bd5e404bb9cc8d6aa5900675485ff763772fa1d
Farah <obaidatfarah4@gmail.com>
2021-06-08 22:50:34 +0300

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+button.onclick=function (){
+fetch(`https://fakerestapi.azurewebsites.net/api/v1/Authors`).then (response => response.json()).
+then(result => {for(let i=0;i<10;i++){
+    authors.insertAdjacentHTML("beforeend", `<div class="author">
+    <h3>
+    ${result[i].firstName},${result[i].lastName}
+    </h3>
+    <h4>
+        ${result[i].idBook}
+    </h4>
+    <button  id="button${i}" >Delete</button>
+</div>`);   
+
+document.getElementsByClassName("author")[i].style.border="red solid 3px";
+document.getElementById(`button${i}`).onclick=function(){document.getElementById(`button${i}`).parentNode.remove()}
+}});
+
+}
